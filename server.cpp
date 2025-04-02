314aa6650c27c14477efe8c5af952dda6826fada
AdiX <131326798+Adityasinghvats@users.noreply.github.com>
2025-04-02 22:57:52 +0530

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -15,7 +15,7 @@
 using namespace std;
 int main() {
 
-    //link the dll
+    //link the dll step 1
     WSADATA wsadata;
     int wsaerr;
     WORD wVersionRequested = MAKEWORD(2, 2);
@@ -29,7 +29,7 @@ int main() {
         cout << "Status: " << wsadata.szSystemStatus << endl;
     }
 
-    //socket creation
+    //socket creation step 2
     SOCKET serversocket = INVALID_SOCKET;
     serversocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 
@@ -43,7 +43,7 @@ int main() {
     }
     //closesocket(serversocket);
 
-    //bind
+    //bind step 3
     int port = 55555;
     sockaddr_in service;
     service.sin_family = AF_INET;
@@ -59,13 +59,13 @@ int main() {
         cout << "bind() is OK" << endl;
     }
 
-    //Listen on socket
+    //Listen on socket step 4
     if (listen(serversocket, 1) == SOCKET_ERROR) {
         cout << "listen() : error listening on socket" << WSAGetLastError() << endl;
     }
     else cout << "Listen() is OK, I'm waiting for connections..." << endl;
 
-    //accept the connection
+    //accept the connection step 5
     SOCKET acceptSocket;
     acceptSocket = accept(serversocket, nullptr, nullptr);
     if (acceptSocket == INVALID_SOCKET) {
@@ -75,9 +75,25 @@ int main() {
     }
     else {
         cout << "accept() is working" << endl;
-        cout << "Client is conencted" << endl;
+        cout << "Client is connected" << endl;
     }
+    //step 6 chat to client
+    char buffer[200];
+    int byteCount = recv(acceptSocket, buffer, 200, 0);
+    if (byteCount > 0) {
+        cout << "Message received: " << buffer << endl;
+    }
+    else WSACleanup();
+
+    //step 7 send confirmation
+    char confirmation[200] = "Message Received";
+    byteCount = send(acceptSocket, confirmation, 200, 0);
+    if (byteCount > 0) {
+        cout << "Confirmation Message Sent: " << confirmation << endl;
+    }
+    else WSACleanup();
     system("pause");
     WSACleanup();
+    cout << "Closed Socket" << endl;
     return 0;
 }
